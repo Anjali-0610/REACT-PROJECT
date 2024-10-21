e3f1ac3af63e3001cd004a5146bdf78e91013a0c
Anjali <anjaliverma8970@gmail.com>
2024-10-21 16:20:24 +0530

    04 custom react commit 2 inside vite react

--- Anjali-vite/src/App.jsx
+++ Anjali-vite/src/App.jsx
@@ -2,10 +2,11 @@
 import Chai from './Chai'
 function App() {
  
-
+const new_var="variable kaise inject hoga";
   return (
     <>
              <h1>Removed the vite and react logo from it and made it simple</h1>
+             <h1>{new_var}</h1>
             <Chai/>
     </>
   )
--- Anjali-vite/src/main.jsx
+++ Anjali-vite/src/main.jsx
@@ -1,9 +1,55 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+
+function MyAPP(){
+    return (
+      <div>
+        <h1>creating a function named myAPP</h1>
+      </div>
+    )
+}
+
+
+const ReactElement ={
+  type:'a',       /*teels th etype of element i.e anchor tag here*/
+  props:{
+      href:'https://google.com',
+      target:'_blank'
+  },
+  children:'click me to visit google'
+}
+
+const anotherElement=(
+  <h1>
+   <a href="https://google.com" target='_blank'>
+    correct way to give the structure to be added
+  </a>
+  </h1>
+ 
 )
+
+const new_var_react='react ki method mein variable bhi inject kara'
+const new_react_element=React.createElement(
+  'a',
+  {
+    href:'https://google.com', target:'_blank'
+  }
+  ,<h1>react  ko usi ke syntax mein element kacontent diya hai</h1>,
+  new_var_react
+)
+
+createRoot(document.getElementById('root')).render(
+
+// {/* <App/> */}
+/*MyApp()*/
+/*<MyAPP/>*/
+/*ReactElement       object hai esliye brackets ke saath call nahi hoga*/
+// anotherElement
+new_react_element       //react ke method se hi content dena
+  
+)
+
+
